545eb572aa5cb983da2ce4438debc3580fdf9d33
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-07-14 21:07:06 -0700

    Initial commit

---
+++ src/main/java/com/persist/persist/controllers/Test.java
@@ -0,0 +1,12 @@
+package com.persist.persist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+	@GetMapping("/test")
+	public String test() {
+		return "test.jsp";
+	}
+}
